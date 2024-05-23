cd7e99bca083edfdaacadc39f6c0f02ad39ae546
Wongbuer <wongbuer@outlook.com>
2024-05-23 22:21:35 +0800

    docs(pet.sql): 修改建表sql

--- src/main/resources/pet.sql
+++ src/main/resources/pet.sql
@@ -11,10 +11,9 @@
  Target Server Version : 80300
  File Encoding         : 65001
 
- Date: 19/05/2024 23:40:31
+ Date: 23/05/2024 22:20:53
 */
-CREATE DATABASE IF NOT EXISTS `pet` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `pet`;
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -32,7 +31,12 @@ CREATE TABLE `address`  (
   `create_time` datetime NOT NULL DEFAULT 'now()' COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of address
+-- ----------------------------
+INSERT INTO `address` VALUES (1, 1, '四川省', '成都市', '新都区', '西南石油大学', '2024-05-23 22:07:09', NULL);
 
 -- ----------------------------
 -- Table structure for announcement
@@ -46,6 +50,10 @@ CREATE TABLE `announcement`  (
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of announcement
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for comment
@@ -62,6 +70,10 @@ CREATE TABLE `comment`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of comment
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for favorites
 -- ----------------------------
 DROP TABLE IF EXISTS `favorites`;
@@ -72,7 +84,12 @@ CREATE TABLE `favorites`  (
   `create_time` datetime NULL DEFAULT 'now()' COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of favorites
+-- ----------------------------
+INSERT INTO `favorites` VALUES (1, 1, 1, '2024-05-23 20:53:21', NULL);
 
 -- ----------------------------
 -- Table structure for likes
@@ -85,7 +102,12 @@ CREATE TABLE `likes`  (
   `create_time` datetime NULL DEFAULT 'now()' COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of likes
+-- ----------------------------
+INSERT INTO `likes` VALUES (1, 1, 1, '2024-05-23 20:56:05', NULL);
 
 -- ----------------------------
 -- Table structure for orders
@@ -93,16 +115,23 @@ CREATE TABLE `likes`  (
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '订单id',
-  `user_id` bigint NOT NULL COMMENT '用户id',
+  `seller_id` bigint NOT NULL COMMENT '卖家id',
+  `buyer_id` bigint NOT NULL COMMENT '买家id',
   `amount` decimal(10, 2) NOT NULL COMMENT '金额',
   `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '类型',
   `start_time` datetime NOT NULL COMMENT '开始时间',
   `end_time` datetime NOT NULL COMMENT '结束时间',
   `days` int NOT NULL COMMENT '寄养天数',
+  `payment_time` datetime NULL DEFAULT NULL COMMENT '支付时间',
+  `status` int NOT NULL DEFAULT 0 COMMENT '订单状态',
   `create_time` datetime NOT NULL DEFAULT 'now()' COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for post
@@ -112,11 +141,19 @@ CREATE TABLE `post`  (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '帖子id',
   `user_id` bigint NOT NULL COMMENT '用户id',
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '标题',
+  `cover_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '封面图',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '内容',
+  `pet_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '宠物类别',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址信息',
   `create_time` datetime NOT NULL DEFAULT 'now()' COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of post
+-- ----------------------------
+INSERT INTO `post` VALUES (1, 1, '太叼拉', '', '正文内容', '猫', '翻斗花园', '2024-05-23 20:52:57', NULL);
 
 -- ----------------------------
 -- Table structure for role
@@ -128,7 +165,13 @@ CREATE TABLE `role`  (
   `create_time` datetime NOT NULL DEFAULT 'now()' COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, '普通用户', '2024-05-19 23:39:48', NULL);
+INSERT INTO `role` VALUES (2, '管理员', '2024-05-19 23:39:48', NULL);
 
 -- ----------------------------
 -- Table structure for user
@@ -141,12 +184,35 @@ CREATE TABLE `user`  (
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
   `age` int NULL DEFAULT NULL COMMENT '年龄',
   `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',
-  `address_id` bigint NULL DEFAULT NULL COMMENT '地址id',
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像',
   `create_time` datetime NOT NULL DEFAULT 'now()' COMMENT '创建时间',
   `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, '王不二', '0f49c643c75a9f1acbffc721ba000d58f1eca3cc79337dce8993d8c613532aaf', 'x.ohhhz@qq.com', NULL, '18116718132', 'http://dummyimage.com/100x100', '2024-05-19 22:41:57', '2024-05-23 22:08:55');
+INSERT INTO `user` VALUES (2, '赵赵赵', '0f49c643c75a9f1acbffc721ba000d58f1eca3cc79337dce8993d8c613532aaf', NULL, NULL, NULL, NULL, '2024-05-22 22:36:14', NULL);
+
+-- ----------------------------
+-- Table structure for user_address
+-- ----------------------------
+DROP TABLE IF EXISTS `user_address`;
+CREATE TABLE `user_address`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户地址表主键',
+  `user_id` bigint NULL DEFAULT NULL COMMENT '用户id',
+  `address_id` bigint NULL DEFAULT NULL COMMENT '地址id',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_address
+-- ----------------------------
+INSERT INTO `user_address` VALUES (1, 1, 1, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for user_role
@@ -157,5 +223,12 @@ CREATE TABLE `user_role`  (
   `role_id` bigint NOT NULL COMMENT '角色id',
   PRIMARY KEY (`user_id`, `role_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (1, 1);
+INSERT INTO `user_role` VALUES (1, 2);
+INSERT INTO `user_role` VALUES (2, 2);
 
 SET FOREIGN_KEY_CHECKS = 1;
